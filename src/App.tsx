d76c34ade1e92668f8893f4fbc3ab05670811fe8
choiseohyeon <csh200310@naver.com>
2024-01-14 21:32:22 +0900

    [feat] scatter plot 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,31 @@ import BrushTest from "./charts/brushTest/BrushTest";
 import BubblePlot from "./charts/bubblePlot/BubblePlot";
 import Test from "./charts/test";
 import Dashboard from "./workspace/Dashboard";
+import { ScatterPlot } from "./charts/scatterPlot/Scatterplot";
 
 /**
  * routes 배열에 [화면 좌측 서랍에 표시될 이름, route url path, 차트 컴포넌트]를 넣으면 됨
  */
 // eslint-disable-next-line react-refresh/only-export-components
 export const routes: [string, string, JSX.Element][] = [
-  ['home', '', <>좌측 메뉴에서 표시할 차트 클릭</>],
-  ['test페이지', 'test1', <Test />],
-  ['bubblePlot', 'bubbleplot', <BubblePlot />],
-  ['brush 테스트', 'brush', <BrushTest></BrushTest>],
-  ['barPlot', 'bar', <BarPlot></BarPlot>]
-]
+  ["home", "", <>좌측 메뉴에서 표시할 차트 클릭</>],
+  ["test페이지", "test1", <Test />],
+  ["bubblePlot", "bubbleplot", <BubblePlot />],
+  ["brush 테스트", "brush", <BrushTest></BrushTest>],
+  ["barPlot", "bar", <BarPlot></BarPlot>],
+  ["ScatterPlot", "scatterplot", <ScatterPlot></ScatterPlot>],
+];
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard></Dashboard>}> {/* 기본 레이아웃 */}
-          {routes.map(([, path, element]) => <Route path={path} element={element} key={path} />)}
+        <Route path="/" element={<Dashboard></Dashboard>}>
+          {" "}
+          {/* 기본 레이아웃 */}
+          {routes.map(([, path, element]) => (
+            <Route path={path} element={element} key={path} />
+          ))}
         </Route>
       </Routes>
     </>
