238bad4667f6a43aafeaa696c7e035d5f95ad1c0
wj-cosmos57 <wooju_0507@naver.com>
2024-01-23 21:19:37 +0900

    add scatterplot annotation

--- src/charts/scatterPlot/scatterPlot.tsx
+++ src/charts/scatterPlot/scatterPlot.tsx
@@ -1,24 +1,42 @@
 import * as d3 from "d3";
 import { useEffect, useRef } from "react";
 
+/*
+  [ 전체 코드 실행 순서 ]
+  1. ScatterPlot 함수 호출
+  2. useRef 초기화
+  3. useEffect 등록 (호출 X)
+  4. render 함수 초기화 (호출 X)
+  5. jsx return 및 svg DOM 객체를 svgRef에 할당
+  6. 등록했던 useEffect 호출
+  7. render 함수 호출 (차트 그리기)
+*/
+
 export const ScatterPlot = () => {
-  /* 
-  let svgRef라고 할 경우 return을
-  <svg ref={(ref) => {svgRef = ref}}></svg> 로 하면 useRef 사용과 같은 효과 
-  */
+  // let svgRef라고 할 경우 return을 <svg ref={(ref) => {svgRef = ref}}></svg> 로 하면 useRef 사용과 같은 효과
   const svgRef = useRef();
 
   useEffect(() => {
     render();
   }, []);
 
+  /////////////////////////////
+  //////// render 코드 ////////
+  /////////////////////////////
+
   const render = () => {
-    // set the dimensions and margins of the graph
+    ////////////////////////
+    // 차트 상하좌우 여백 //
+    ////////////////////////
     const margin = { top: 10, right: 30, bottom: 30, left: 60 },
-      width = 460 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+      svgWidth = 460,
+      svgHeight = 400,
+      width = svgWidth - margin.left - margin.right,
+      height = svgHeight - margin.top - margin.bottom;
 
     /*
+    d3.select() : 인자로 들어온 값을 선택
+
     d3.select 함수의 인자
     -> object 타입의 경우 DOM 객체
       - document.querySelector로 검색한 DOM
@@ -27,44 +45,67 @@ export const ScatterPlot = () => {
       - #my_dataviz
       - .container
       - div
+
+    return :
+    - 선택한 DOM 객체가 d3.Selection으로 wrapping 되어 반환됨
+    - wrapping되어 반환된 객체는 d3.selection의 메서드들을 사용할 수 있음
+      ( select, append, attr 등등 다시 사용 가능 )
     */
-    d3.select(svgRef.current).select('g').remove(); // react에 의해 중복 생성된 내부 element 제거
-    // append the svg object to the body of the page
+
+    // react에 의해 중복 생성된 내부 element 제거
+    d3.select(svgRef.current).select("g").remove();
+
+    ///////////////////////////////////////////////////
+    // append the svg object to the body of the page //
+    ///////////////////////////////////////////////////
     const svg = d3
       .select(svgRef.current)
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+      .attr("width", svgWidth)
+      .attr("height", svgHeight)
+      .append("g") // 선택된 DOM 객체[코드상 svg]의 자식(인자로 넣은 요소[코드상 g])를 추가하는 함수
+      .attr("transform", `translate(${margin.left}, ${margin.top})`); // transform 속성의 값, margin left&top만큼 이동시킴
+    // 메서드 체이닝에서 최종 return 값은 마지막 사용한 select/append (const svg는 g element가 할당)
 
-    //Read the data
+    ///////////////////
+    // Read the data //
+    ///////////////////
     d3.csv(
       "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv"
     ).then(function (data) {
-      // Add X axis
+      ///////////////////
+      // Add X. Y axis //
+      ///////////////////
       const x = d3.scaleLinear().domain([4, 8]).range([0, width]);
       svg
         .append("g")
         .attr("transform", `translate(0, ${height})`)
-        .call(d3.axisBottom(x));
+        .call(d3.axisBottom(x)); // axisBottom : 수평선-tick이 아래를 향함
 
-      // Add Y axis
       const y = d3.scaleLinear().domain([0, 9]).range([height, 0]);
-      svg.append("g").call(d3.axisLeft(y));
+      svg.append("g").call(d3.axisLeft(y)); // axisLeft : 수직선-tick이 왼쪽을 향함
 
-      // Color scale: give me a specie name, I return a color
+      //////////////////////////////////////////////////////////
+      // Color scale: give me a specie name, I return a color //
+      //////////////////////////////////////////////////////////
       const color = d3
-        .scaleOrdinal<string>()
+        .scaleOrdinal<string>() // color 함수 사용 시에는 타입 에러 방지를 위해 제네릭
         .domain(["setosa", "versicolor", "virginica"])
         .range(["#440154ff", "#21908dff", "#fde725ff"]);
 
-      // Add dots
+      //////////////
+      // Add dots //
+      //////////////
+      // [ selectAll - data - json ]
+      // 데이터 등록(첫 호출)을 통한 요소들의 삽입과 데이터 업데이트(두번째 호출부터)를 통한 요소들의 수정을 겸함
+      // selectAll : 업데이트 대상 식별
+      // data - json : data에 등록된 배열요소를 전부 circle로 만들기
       svg
-        .append("g")
-        .selectAll("dot")
-        .data(data)
-        .join("circle")
+        .append("g") // svg(container)에 그룹(g)을 추가
+        .selectAll("dot") // "dot"이라는 selector를 전부 선택
+        .data(data) // 데이터 등록 (두번째 인자 key 전달, 없으면 index 기준 업데이트)
+        .join("circle") // 등록한 배열의 length만큼 circle 요소 생성
         .attr("cx", function (d) {
+          // .attr() : 속성 설정
           return x(+d.Sepal_Length);
         })
         .attr("cy", function (d) {
@@ -72,12 +113,12 @@ export const ScatterPlot = () => {
         })
         .attr("r", 5)
         .style("fill", function (d) {
+          // .style() : css 설정
           return color(d.Species);
         });
     });
   };
 
   // jsx에 만든 svg를 useRef를 통해 그 svg DOM 객체를 svgRef에 할당
-  return <svg ref={svgRef}></svg>
-  ;
+  return <svg ref={svgRef}></svg>;
 };
