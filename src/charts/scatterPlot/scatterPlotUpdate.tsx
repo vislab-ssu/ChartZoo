1ddb3a7414a9fcd1d735c1d0fc044387ec0add26
wj-cosmos57 <wooju_0507@naver.com>
2024-02-11 01:16:56 +0900

    Comment: scatterPlotUpdate.tsx

--- src/charts/scatterPlot/scatterPlotUpdate.tsx
+++ src/charts/scatterPlot/scatterPlotUpdate.tsx
@@ -1,6 +1,7 @@
 import * as d3 from "d3";
 import { useEffect, useRef, useState } from "react";
 
+// 사용할 데이터셋 url 목록
 const urlList = [
   "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv",
   "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/4_ThreeNum.csv",
@@ -16,12 +17,31 @@ export const ScatterPlotUpdate = () => {
   // svg DOM 객체
   const svgRef = useRef();
 
-  // useEffect : 초기화 후 한번 호출 (레이아웃만 그려줌[크기, margin, 축 등])
+  /* 
+    [ 실행 흐름 순서 ]
+    1. 첫 실행
+    2. 전부 초기화
+    3. useEffect 호출 -> renderLayou 호출
+    4. 정적인 부분 생성
+    5. useEffect 호출 -> d3.csv(dataUrL) 호출
+    6. .then(updateRef.current) 호출
+    7. 동적인 부분 생성
+
+    이후 change 버튼을 누를 때마다...
+    1. setDataUrl 호출 및 dataUrl 변경
+    2. re-render
+    3. useEffect 호출 -> d3.csv(dataUrl) 호출
+    4. .then(updateRef.current) 호출
+    5. 동적인 부분 업데이트
+  */
+
+  // useEffect : 초기화 후 한번 호출 => (레이아웃만 그려줌[크기, margin, 축 등])
   useEffect(() => {
     renderLayout();
   }, []);
 
   // useEffect : 데이터셋이 바뀔 때마다 호출
+  // => d3.csv 함수로 csv 파일 parsing 하고 updateRef 함수 호출
   useEffect(() => {
     console.log(dataUrl);
     d3.csv(dataUrl).then(updateRef.current);
@@ -43,9 +63,9 @@ export const ScatterPlotUpdate = () => {
       .attr("class", "container")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    /////////////////////////////
+    ///////////////////////
     // 변하지 않는 정적인 부분 //
-    /////////////////////////////
+    ///////////////////////
 
     // scale 함수(변하지 않는 정적인 부분)
     // 결과값인 range만 설정
@@ -69,15 +89,15 @@ export const ScatterPlotUpdate = () => {
     // scatter그룹도 그룹만 설정하고 끝
     const scatters = container.append("g").attr("class", "scatters");
 
-    //////////////////////////////
+    ////////////////////////
     // 변할 수 있는 동적인 부분 //
-    //////////////////////////////
+    ////////////////////////
     // (scale함수의 domain 설정/변경, scatter 생성/업데이트 등)
 
     updateRef.current = function (data) {
-      ///////////////////////////////////////////////////////////
+      //////////////////////////////////////////////////
       // dataset의 변경에 따라 x, y축 key가 달라지는 문제 해결 //
-      ///////////////////////////////////////////////////////////
+      //////////////////////////////////////////////////
       const keys = Object.keys(data[0]);
       console.log({ keys });
       // Object.keys 함수로 key 추출 및 xKey, yKey, color로 구조 분해 할당
