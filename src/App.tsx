1515624f286d3c78c873ef09e1c2bdb737dcbad5
wj-cosmos57 <wooju_0507@naver.com>
2024-02-14 13:01:48 +0900

    task3: SyncBarPlot

--- src/App.tsx
+++ src/App.tsx
@@ -6,27 +6,33 @@ import BubblePlot from "./charts/bubblePlot/BubblePlot";
 import { ScatterPlotUpdate } from "./charts/scatterPlot/scatterPlotUpdate";
 import Test from "./charts/test";
 import Dashboard from "./workspace/Dashboard";
+import { SyncBarWorkspace } from "./charts/SyncBarPlot";
 
 /**
  * routes 배열에 [화면 좌측 서랍에 표시될 이름, route url path, 차트 컴포넌트]를 넣으면 됨
  */
 // eslint-disable-next-line react-refresh/only-export-components
 export const routes: [string, string, JSX.Element][] = [
-  ['home', '', <>좌측 메뉴에서 표시할 차트 클릭</>],
-  ['test페이지', 'test1', <Test />],
-  ['bubblePlot', 'bubbleplot', <BubblePlot />],
-  ['brush 테스트', 'brush', <BrushTest></BrushTest>],
-  ['barPlot', 'bar', <BarPlotUpdate></BarPlotUpdate>],
-  ['scatterPlot', 'scatter', <ScatterPlotUpdate></ScatterPlotUpdate>],
-  ['selectableBarPlot', 'selectableBar', <Workspace></Workspace>]
-]
+  ["home", "", <>좌측 메뉴에서 표시할 차트 클릭</>],
+  ["test페이지", "test1", <Test />],
+  ["bubblePlot", "bubbleplot", <BubblePlot />],
+  ["brush 테스트", "brush", <BrushTest></BrushTest>],
+  ["barPlot", "bar", <BarPlotUpdate></BarPlotUpdate>],
+  ["scatterPlot", "scatter", <ScatterPlotUpdate></ScatterPlotUpdate>],
+  ["selectableBarPlot", "selectableBar", <Workspace></Workspace>],
+  ["syncBarPlot", "syncBar", <SyncBarWorkspace></SyncBarWorkspace>],
+];
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Dashboard></Dashboard>}> {/* 기본 레이아웃 */}
-          {routes.map(([, path, element]) => <Route path={path} element={element} key={path} />)}
+        <Route path="/" element={<Dashboard></Dashboard>}>
+          {" "}
+          {/* 기본 레이아웃 */}
+          {routes.map(([, path, element]) => (
+            <Route path={path} element={element} key={path} />
+          ))}
         </Route>
       </Routes>
     </>
